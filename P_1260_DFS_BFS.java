50e12995c185d86bc61e6576a4d27cc24bdb3755
NerdyJJ <94119354+NerdyJJ@users.noreply.github.com>
2022-01-31 16:19:47 +0900

    Add files via upload

---
+++ P_1260_DFS_BFS.java
@@ -0,0 +1,88 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class P_1260_DFS_BFS {
+	
+	// 그래프를 DFS로 탐색한 결과와 BFS로 탐색한 결과를 출력하는 프로그램을 작성하시오.
+	// 단, 방문할 수 있는 정점이 여러 개인 경우에는 정점 번호가 작은 것을 먼저 방문하고, 더 이상 방문할 수 있는 점이 없는 경우 종료한다.
+	// 정점 번호는 1번부터 N번까지이다.
+	
+	// 입력
+	// 첫째 줄에 정점의 개수 N(1 ≤ N ≤ 1,000), 간선의 개수 M(1 ≤ M ≤ 10,000), 탐색을 시작할 정점의 번호 V가 주어진다.
+	// 다음 M개의 줄에는 간선이 연결하는 두 정점의 번호가 주어진다.
+	// 어떤 두 정점 사이에 여러 개의 간선이 있을 수 있다.
+	// 입력으로 주어지는 간선은 양방향이다.
+
+	// 출력
+	// 첫째 줄에 DFS를 수행한 결과를, 그 다음 줄에는 BFS를 수행한 결과를 출력한다.
+	// V부터 방문된 점을 순서대로 출력하면 된다.
+	
+	
+	static int[][] graph = new int[1001][1001];
+	static List<Integer> queue = new ArrayList<>();
+	static int[] visited = new int[1001];
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] input = br.readLine().split(" ");
+		int N = Integer.parseInt(input[0]);
+		int M = Integer.parseInt(input[1]);
+		int V = Integer.parseInt(input[2]);
+
+		graph = new int[N + 1][N + 1];
+		for (int i = 0; i < M; i++) {
+			String[] edge = br.readLine().split(" ");
+			int x = Integer.parseInt(edge[0]);
+			int y = Integer.parseInt(edge[1]);
+
+			graph[x][y] = graph[y][x] = 1;
+		}
+		
+		dfs(V);
+		for (int i = 0; i < visited.length; i++) {
+			visited[i] = 0;
+		}
+		System.out.println();
+		bfs(V);
+	}
+	
+	static void dfs(int v) {
+		if (visited[v] == 1) {
+			return;
+		}
+		if (visited[v] == 0) {
+			System.out.print(v + " ");
+			visited[v] = 1;
+		}
+		
+		for (int i = 0; i < graph.length; i++) {
+			if (graph[v][i] == 1) {
+				dfs(i);
+			}
+		}
+	}
+	
+	static void bfs(int v) {
+		queue.add(v);
+		while(!queue.isEmpty()) {
+			v = queue.get(0);
+			if(visited[v] == 0) {
+				System.out.print(v + " ");
+				visited[v] = 1;
+			}
+			
+			for (int i = 0; i < graph.length; i++) {
+				if (graph[v][i] == 1 && visited[i] == 0) {
+					queue.add(i);
+					visited[i] = 1;
+					System.out.print(i + " ");
+				}
+			}
+			queue.remove(0);
+		}
+	}
+
+}
---
+++ P_19621_MeetingRoom3.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P_19621_MeetingRoom3 {
+	
+	// 서준이는 아빠로부터 N개의 회의와 하나의 회의실을 선물로 받았다.
+	// 각 회의는 시작 시간, 끝나는 시간, 회의 인원이 주어지고 한 회의실에서 동시에 두 개 이상의 회의가 진행될 수 없다.
+	// 단, 회의는 한번 시작되면 중간에 중단될 수 없으며 한 회의가 끝나는 것과 동시에 다음 회의가 시작될 수 있다.
+	// 회의의 시작 시간은 끝나는 시간보다 항상 작다.
+	// N개의 회의를 회의실에 효율적으로 배정할 경우 회의를 진행할 수 있는 최대 인원을 구하자.
+	
+	// 입력
+	// 첫째 줄에 회의의 수 N이 주어진다.
+	// 둘째 줄부터 N + 1 줄까지 공백을 사이에 두고 회의의 시작시간, 끝나는 시간, 회의 인원이 주어진다.
+
+	// 출력
+	// 첫째 줄에 회의실에서 회의를 진행할 수 있는 최대 인원을 출력한다.
+	
+	// 제한
+	// 1 ≤ N ≤ 100,000
+	// 임의의 회의 K(1≤ K ≤ N)는 회의 K − 1과 회의 K + 1과는 회의 시간이 겹치고 다른 회의들과는 회의 시간이 겹치지 않는다.
+	// 모든 회의의 시작 시간과 끝나는 시간은 231 − 1보다 작거나 같은 자연수 또는 0이다.
+	// 모든 회의의 시작 시간과 끝나는 시간은 서로 다르다.
+	// 회의 인원은 1,000보다 작거나 같은 자연수 이다.
+	
+	
+	static int[] time = new int[100000];
+	static int[] max = new int[100000];
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < N; i++) {
+			time[i] = Integer.parseInt(br.readLine().split(" ")[2]);
+		}
+		
+		// 입력완료
+		
+//		N=1, t1 = 1
+//		N=2, t2 = max(t1, 2)
+//		N=3, t3 = max(t2, t1 + 3)
+//		N=4, t4 = max(t3, t2 + 4)
+//		N=5, t5 = max(t4, t3 + 5) ...
+		
+		for (int i = 0; i < N; i++) {
+			if (i == 0) {
+				max[i] = time[i];
+			} else if (i == 1) {
+				max[i] = Math.max(max[i-1], time[i]);
+			} else {
+				max[i] = Math.max(max[i-1], max[i-2]+time[i]);
+			}
+		}
+		
+		System.out.println(max[N-1]);
+	}
+}
